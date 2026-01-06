bc8bcd2c02fc9358c231369530812907cde03c1e
Sooraj P <soorajchemmanoor@gmail.com>
2026-01-06 21:46:17 +0530

    keyup event added

--- javascript/practice/js-app4.js
+++ javascript/practice/js-app4.js
@@ -1,8 +1,14 @@
 // onchange
-let inputs = document.querySelectorAll('input');
-inputs.forEach((input)=>{
-    input.addEventListener('change', ()=>{
-        document.getElementById('words').innerText = input.value;
-    })
-})
+// let inputs = document.querySelectorAll('input');
+// inputs.forEach((input)=>{
+//     input.addEventListener('change', ()=>{
+//         document.getElementById('words').innerText = input.value;
+//     })
+// })
 
+// onkeyup and onkeydown
+
+let input = document.querySelector('input');
+input.addEventListener('keyup', ()=>{
+     document.getElementById('words').innerText = input.value;
+})
