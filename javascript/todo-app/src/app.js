ca7a8e9518c541a1f6d0ed9e4c0df579276c4b79
Sooraj P <soorajchemmanoor@gmail.com>
2026-01-12 19:26:50 +0530

    todo app : add task functino added

--- javascript/todo-app/src/app.js
+++ javascript/todo-app/src/app.js
@@ -1,9 +1,21 @@
 let ToDos = [];
-let i=0;
+
 document.querySelector('#showAddTask').addEventListener('click',()=>{
     let addTaskContainer = document.getElementById('addTaskContainer');
     addTaskContainer.classList.toggle('opacity-0'); 
     addTaskContainer.classList.toggle('scale-0');
     addTaskContainer.classList.toggle('pointer-events-none')
     
+});
+
+document.querySelector('#addTask').addEventListener('click',()=>{
+    let taskName = document.getElementById('taskName').value; 
+    let date = document.getElementById("date").value; 
+    let taskDetails = {
+        'name':taskName,
+        'date':date
+    };
+    console.log(date);
+    ToDos.push(taskDetails);
+    console.log(ToDos);
 });
