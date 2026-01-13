977eb7dee77a7b047e988afdd5d0d9b010192775
Sooraj P <soorajchemmanoor@gmail.com>
2026-01-13 17:12:26 +0530

    todo app : rendering todos added

--- javascript/todo-app/src/app.js
+++ javascript/todo-app/src/app.js
@@ -1,7 +1,9 @@
 let ToDos = [];
 
+const toDoList = document.querySelector('#toDoList');
 const toast = document.getElementById("todo-alert");
-let toastMsg = ""
+let toastMsg = "";
+
 
 if(!localStorage.getItem("ToDos"))
 {
@@ -13,6 +15,51 @@ else{
    
 }
 
+rederToDos()
+
+
+
+function rederToDos(){
+    toDoList.innerHTML="";
+    ToDos.forEach((todo,index)=>{
+
+        if(todo.status == "done"){
+            todoBgClass = "bg-red-100 hover:shadow-md";
+            todoNameClass = "text-neutral-700";
+        }
+        else{
+            todoBgClass = "border-orange-400";
+            todoNameClass = "text-orange-700";
+        }
+
+        let todoStruct = `<div class="w-[335px] border  p-3  rounded-xl  hover:cursor-pointer ${todoBgClass}">
+            <p class=" font-semibold ${todoNameClass}" >${todo.name}</p>
+            <p class="text-[14px]  text-neutral-900 mt-1">${todo.date}</p>
+            <div class="flex gap-3">
+                <button todo-id="${index}" class="text-neutral-700 text-sm  hover:text-green-700 flex gap-2 mt-3 items-center" >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-circle" viewBox="0 0 16 16">
+                        <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0"/>
+                        <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"/>
+                    </svg>
+                    ${(todo.status == "done") ? "Mark as Incomplete" : "Mark as Complete"}
+                </button>
+                <button class="text-neutral-700 text-sm  hover:text-red-500 flex gap-2 mt-3 items-center" >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                        <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+                    </svg>
+                    Delete
+                </button>
+            </div>
+
+        </div>`;
+
+        toDoList.innerHTML = toDoList.innerHTML +todoStruct;
+
+
+    })
+}
+
+
 
 document.querySelector('#showAddTask').addEventListener('click',()=>{
     let addTaskContainer = document.getElementById('addTaskContainer');
@@ -22,12 +69,17 @@ document.querySelector('#showAddTask').addEventListener('click',()=>{
     
 });
 
+
+
+
+
 document.querySelector('#addTask').addEventListener('click',()=>{
     let taskName = document.getElementById('taskName').value; 
     let date = document.getElementById("date").value; 
     let taskDetails = {
         'name':taskName,
-        'date':date
+        'date':date,
+        'status':'pending'
     };
 
   
@@ -73,7 +125,7 @@ document.querySelector('#addTask').addEventListener('click',()=>{
         }, 3000);
 
         console.log(JSON.parse(localStorage.getItem("ToDos")));
-
+        rederToDos();
 
     }
     else{
