2bd69a43221fcb48c34bb01ba6401f03ec23b927
Sooraj P <soorajchemmanoor@gmail.com>
2026-01-13 17:50:30 +0530

    todo app : todo deleting optino added

--- javascript/todo-app/src/app.js
+++ javascript/todo-app/src/app.js
@@ -2,6 +2,7 @@ let ToDos = [];
 
 const toDoList = document.querySelector('#toDoList');
 const toast = document.getElementById("todo-alert");
+
 let toastMsg = "";
 
 
@@ -28,7 +29,7 @@ function rederToDos(){
             todoNameClass = "text-neutral-700";
         }
         else{
-            todoBgClass = "border-orange-400";
+            todoBgClass = "border-orange-400 hover:bg-orange-100";
             todoNameClass = "text-orange-700";
         }
 
@@ -43,7 +44,7 @@ function rederToDos(){
                     </svg>
                     ${(todo.status == "done") ? "Mark as Incomplete" : "Mark as Complete"}
                 </button>
-                <button class="text-neutral-700 text-sm  hover:text-red-500 flex gap-2 mt-3 items-center" >
+                <button todo-id="${index}" class="taskDelete text-neutral-700 text-sm  hover:text-red-500 flex gap-2 mt-3 items-center" >
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
                         <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
                     </svg>
@@ -57,9 +58,102 @@ function rederToDos(){
 
 
     })
+    const taskDelete = document.querySelectorAll('.taskDelete');
+    taskDelete.forEach(
+        (tDel)=>{
+            tDel.addEventListener('click',()=>{
+                id = tDel.getAttribute('todo-id')
+                deleteTask(id);
+            })
+        }
+    );
 }
 
+function deleteTask(id){
+    if(ToDos.length > 0)
+    {
+        ToDos.splice(id,1);
+        localStorage.setItem("ToDos", JSON.stringify(ToDos));
+        icon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill text-green-500 " viewBox="0 0 16 16"> <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/></svg>'
+        successToast('ToDo Deleted Successfully!',icon);
+        rederToDos();
+    }
 
+}
+
+function successToast(message,icon){
+
+        toastMsg = message;
+        toast.innerHTML = `${icon} <p class="text-green-600">${toastMsg}</p>`;
+
+        toast.classList.remove(
+            "opacity-0",
+            "translate-x-6",
+            "scale-95",
+            "pointer-events-none",
+            "bg-red-200"
+        );
+        toast.classList.add(
+            "opacity-100",
+            "translate-x-0",
+            "scale-100",
+            "bg-green-200"
+        );
+
+        setTimeout(() => {
+            toast.classList.add(
+                "opacity-0",
+                "translate-x-6",
+                "scale-95",
+                "pointer-events-none",
+                "bg-green-200"
+            );
+            toast.classList.remove(
+                "opacity-100",
+                "translate-x-0",
+                "scale-100",
+                "bg-red-200"
+            );
+        }, 3000);
+
+}
+
+function dangerToast(message,icon){
+
+        toastMsg = message;
+        toast.innerHTML = `${icon} <p class="text-green-600">${toastMsg}</p>`;
+
+        toast.classList.remove(
+            "opacity-0",
+            "translate-x-6",
+            "scale-95",
+            "pointer-events-none",
+            "bg-green-200"
+        );
+        toast.classList.add(
+            "opacity-100",
+            "translate-x-0",
+            "scale-100",
+            "bg-red-200"
+        );
+
+        setTimeout(() => {
+            toast.classList.add(
+                "opacity-0",
+                "translate-x-6",
+                "scale-95",
+                "pointer-events-none",
+                "bg-red-200"
+            );
+            toast.classList.remove(
+                "opacity-100",
+                "translate-x-0",
+                "scale-100",
+                "bg-green-200"
+            );
+        }, 3000);
+
+}
 
 document.querySelector('#showAddTask').addEventListener('click',()=>{
     let addTaskContainer = document.getElementById('addTaskContainer');
@@ -83,46 +177,15 @@ document.querySelector('#addTask').addEventListener('click',()=>{
     };
 
   
-
-    let success = '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-check-circle-fill  text-green-500 " viewBox="0 0 16 16"> <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/></svg>';
-    let danger = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-ban-fill text-red-500" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M2.71 12.584q.328.378.706.707l9.875-9.875a7 7 0 0 0-.707-.707l-9.875 9.875Z"/></svg>';
     ToDos.push(taskDetails);
     localStorage.setItem("ToDos", JSON.stringify(ToDos));
 
     if (ToDos.length > 0) 
     {
+
         toastMsg = "ToDo Added Successfully!";
-        toast.innerHTML = `${success} <p class="text-green-600">${toastMsg}</p>`;
-
-        toast.classList.remove(
-            "opacity-0",
-            "translate-x-6",
-            "scale-95",
-            "pointer-events-none",
-            "bg-red-200"
-        );
-        toast.classList.add(
-            "opacity-100",
-            "translate-x-0",
-            "scale-100",
-            "bg-green-200"
-        );
-
-        setTimeout(() => {
-            toast.classList.add(
-                "opacity-0",
-                "translate-x-6",
-                "scale-95",
-                "pointer-events-none",
-                "bg-green-200"
-            );
-            toast.classList.remove(
-                "opacity-100",
-                "translate-x-0",
-                "scale-100",
-                "bg-red-200"
-            );
-        }, 3000);
+        let success = '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-check-circle-fill  text-green-500 " viewBox="0 0 16 16"> <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/></svg>';
+        successToast(toastMsg,success);
 
         console.log(JSON.parse(localStorage.getItem("ToDos")));
         rederToDos();
@@ -130,39 +193,13 @@ document.querySelector('#addTask').addEventListener('click',()=>{
     }
     else{
         toastMsg = "Error in Adding ToDo";
-        toast.innerHTML = `${danger} <p class="text-red-600">${toastMsg}</p>`;
-
-        toast.classList.remove(
-            "opacity-0",
-            "translate-x-6",
-            "scale-95",
-            "pointer-events-none",
-            "bg-green-200"
-        );
-        toast.classList.add(
-            "opacity-100",
-            "translate-x-0",
-            "scale-100",
-            "bg-red-200"
-        );
-
-        setTimeout(() => {
-            toast.classList.add(
-                "opacity-0",
-                "translate-x-6",
-                "scale-95",
-                "pointer-events-none",
-                "bg-red-200"
-            );
-            toast.classList.remove(
-                "opacity-100",
-                "translate-x-0",
-                "scale-100",
-                "bg-green-200"
-            );
-        }, 3000);
+        let danger = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-ban-fill text-red-500" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M2.71 12.584q.328.378.706.707l9.875-9.875a7 7 0 0 0-.707-.707l-9.875 9.875Z"/></svg>';
+    
+        successToast(toastMsg,danger);
+        rederToDos();
 
     }
+    
     
 
     
