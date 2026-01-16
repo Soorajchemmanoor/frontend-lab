36f154ed86324fcc6b11b6cb1a888a2d501c4e9b
Sooraj P <soorajchemmanoor@gmail.com>
2026-01-16 23:02:48 +0530

    typeof tested with multiple data types

--- javascript/practice/js-practice7.js
+++ javascript/practice/js-practice7.js
@@ -6,12 +6,22 @@ console.log(indDay1);
 const indDay2 = new Date(1947, 7,15).toString();
 console.log(indDay2);
 
-let opt = confirm("Do you want see the indian indipendace day");
+// let opt = confirm("Do you want see the indian indipendace day");
 
-if(opt)
-{
-    let name = prompt('Enter your name?');
-    alert(`hi ${name}! ${indDay2}`);
-}
-else
-    alert("Bie! thanks for the confirmatin");
+// if(opt)
+// {
+//     let name = prompt('Enter your name?');
+//     alert(`hi ${name}! ${indDay2}`);
+// }
+// else
+//     alert("Bie! thanks for the confirmatin");
+
+
+const ob = {"name":"sooraj"};
+const arr = ["a","b",5];
+console.log(typeof null);
+console.log(typeof arr );
+console.log(typeof ob);
+console.log(typeof undefined);
+console.log(typeof true);
+console.log(typeof "undefined");
