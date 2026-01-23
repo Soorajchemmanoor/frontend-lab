7ad06b00c326f716d4c7fb1ce53a587e784c27ca
Sooraj P <soorajchemmanoor@gmail.com>
2026-01-23 15:01:44 +0530

    components practiced

---
+++ react-js/react-sample/reacting/src/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar(){
+    return (
+        <>
+            <div className="navbar">
+                <h1>ReactFacts</h1>
+                <h2>React Course - Project 1</h2>
+
+            </div>
+        </>
+    )
+}
+
+export default Navbar;
