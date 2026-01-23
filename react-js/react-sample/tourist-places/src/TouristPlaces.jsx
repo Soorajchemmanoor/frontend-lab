8c3bcd7ed817173b78e79bc433b4ae614737f48f
Sooraj P <soorajchemmanoor@gmail.com>
2026-01-24 00:45:39 +0530

    bg highliting code modified when the season turns to summer and winter.

--- react-js/react-sample/tourist-places/src/TouristPlaces.jsx
+++ react-js/react-sample/tourist-places/src/TouristPlaces.jsx
@@ -3,11 +3,11 @@ function TouristPlaces({ img, name, description, season, rating, price }) {
 
     return (
         <>
-            <div className="w-[300px] mx-auto px-4 sm:px-6 lg:px-3 py-3 bg-green-100 rounded-lg shadow-md">
+            <div className={`w-[300px] mx-auto px-4 sm:px-6 lg:px-3 py-3 rounded-lg shadow-md ${season === 'Winter' ? 'bg-red-50' : season === 'Summer' ? 'bg-green-50' : 'bg-white'} `} >
                 <div className="relative">
                     <img className="w-full rounded-lg h-48 object-cover" src={img} alt="" />
                     <p className="absolute bottom-2 right-2 px-2 rounded-full bg-white font-10 ">₹{price}</p>
-                    {season === "Winter" ? <p className="absolute bottom-2 left-2 px-2 rounded-full bg-red-500 text-white font-10 ">Best time: Winter</p> : <p className="absolute bottom-2 left-2 p-0 px-2  rounded-full bg-green-600 text-white font-10 ">Best time: Summer</p> }
+                    <p className={`absolute bottom-2 left-2 px-2 rounded-full  font-10 ${season === "Winter" ?  'bg-red-600 text-white' : season === "Summer" ? 'bg-green-600 text-white' : 'bg-white text-black'} `}>{season}</p>  
                     { price < 12000 ? <p className="absolute top-2 right-2 px-2 rounded-full bg-yellow-200 font-10 ">Cheaper</p> : <p className="absolute top-2 right-2 px-2  rounded-full bg-orange-200 font-10 ">Expensive</p> }
                 </div>
                 <div className="flex justify-between items-center mt-4">
