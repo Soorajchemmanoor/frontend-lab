50ae95bb2e9c5428c757520d19ebb5f5c0d76ed6
Sooraj P <soorajchemmanoor@gmail.com>
2026-01-23 21:29:20 +0530

    onClick Alert Added

--- react-js/react-sample/tourist-places/src/TouristPlaces.jsx
+++ react-js/react-sample/tourist-places/src/TouristPlaces.jsx
@@ -17,6 +17,10 @@ function TouristPlaces({ img, name, description, season, rating, price }) {
                 </div>
 
                 <p className="mt-2">{description}</p>
+                <button className="mt-2 px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700" onClick={
+                    () => alert(`You have selected ${name} as your tourist destination!`)
+                }
+                   >Explore</button>
             </div>
         </>
     );
