ab32c7a418805f12e23097536feda24d893ed370
Sooraj P <soorajchemmanoor@gmail.com>
2026-01-25 23:23:37 +0530

    alert component added

--- react-js/react-sample/reacting/src/App.jsx
+++ react-js/react-sample/reacting/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import Counter from './components/Counter';
+import Alert from './components/Alert';
 import Navbar from './Navbar';
 
 
@@ -13,6 +14,7 @@ function App(){
       <h1 className='text-3xl font-bold underline'>This is my first component {1 + 1}</h1>
       <p className='bg-blue-500 text-white p-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et debitis error enim nostrum!</p>
       <Counter />
+      <Alert />
     </> 
   )
 
