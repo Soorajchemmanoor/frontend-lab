186a352d8ab543b80613a369e2d8466fb42f4dbe
Sooraj P <soorajchemmanoor@gmail.com>
2026-01-26 22:52:34 +0530

    Basic Boiler Plate created for Insta Dupe

---
+++ react-js/insta-dupe/src/App.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+
+function App(){
+
+  return (
+    <>
+
+      <Header />
+      <Routes>
+        
+      </Routes>
+      <Footer />
+      
+     
+    </> 
+  )   
+}
+export default App;
---
+++ react-js/insta-dupe/src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <>
+            <header className="bg-white border-b border-gray-300 p-4 flex justify-between items-center">
+                <h1 className="text-2xl font-bold text-gray-800">InstaDupe</h1>
+            </header>
+        </>
+    );
+}
+
+export default Header;
