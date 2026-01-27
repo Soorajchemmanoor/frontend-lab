993894f4d02fa189a1cc9d17af3b1f6dbad7f2b2
Sooraj P <soorajchemmanoor@gmail.com>
2026-01-27 23:25:45 +0530

    post page modified

--- react-js/insta-dupe/src/pages/Post.jsx
+++ react-js/insta-dupe/src/pages/Post.jsx
@@ -6,48 +6,62 @@ function Post(){
     const user = users.find(u => u.username === username);
     const post = user.posts.find(p => p.id === parseInt(id));
     
+    const options = {
+        
+        month: 'long',
+        day: 'numeric',
+    };
 
 
     return (
         <>
-            <div className="mx-auto max-w-xl p-4">
-                <div className="flex justify-between items-center mb-4">
-                    <h1 className="text-3xl font-bold mb-4">Post by {user.name}</h1>
+            
+            
+            <div className="mx-auto max-w-xl p-4 my-8">
+
+                <div className="flex justify-between items-center gap-4 mt-4  mb-4">
+                    <div className="flex items-center gap-2">
+                        <img src={user.image} alt="" className="w-16 h-16 rounded-full object-cover" />
+                        <div className="text-left" >
+                            <h2 className="text-xl font-bold">{user.name} <span className="text-sm text-neutral-400">@{user.username}</span> </h2>
+                            <p className="text-gray-700 ">{user.profession}</p>
+                            
+                        </div>
+                    </div>
                     <Link to={`/profile/${user.username}`} className="text-white hover:underline bg-black p-2 rounded">
                         Back to Profile
                     </Link>
                 </div>
                 
-                <img src={post.image} alt={post.caption} className="w-full h-auto object-cover mb-4 border-8 border-white shadow-xl " />
-                <p className="text-gray-700">{post.caption}</p>
-                <div className="flex justify-start  items-center gap-8 mt-4">
-                    
-                    
-                    <div>
-                         
-                        <span>{post.posted}</span>
-                    </div>
-
-                    <div>
-                        <span className="font-bold">{post.likes}</span>
-                        <span className="text-gray-500 ml-1">Likes</span>
-                    </div>
-                    
                 
+                <img src={post.image} alt={post.caption} className="w-full h-auto object-cover mb-4  border-white shadow-xl " />
+                <p className="mt-2">{post.caption}</p>
+
+                <div className="flex mt-4 justify-between">
+                    <div className="flex gap-4" >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-chat-left" viewBox="0 0 16 16">
+                            <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                        </svg> 
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
+                            <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z"/>
+                        </svg>
+                    </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-bookmark" viewBox="0 0 16 16">
+                    <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1z"/>
+                    </svg>
                 </div>
+
+                <p className="font-bold mt-2">{post.likes} Likes</p>
+                <p>{(new Date(post.posted)).toLocaleDateString('en', options)}</p>
 
                 
                 
                 
             </div>
-            <div className="mx-auto max-w-xl p-4 flex items-center gap-4   mb-24 bg-neutral-100">
-                <img src={user.image} alt="" className="w-24 h-24 rounded-full object-cover" />
-                <div className="text-left" >
-                    <h2 className="text-2xl font-bold">{user.name}</h2>
-                    <p className="text-gray-700 mt-1">{user.profession}</p>
-                    <p className="text-gray-700">{user.description}</p>
-                </div>
-            </div>
+            
                
         </>
     )
