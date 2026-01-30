81b5b13cd7572f7fb67cd216f6d75a68a0991a51
Sooraj P <soorajchemmanoor@gmail.com>
2026-01-30 23:53:23 +0530

    Basic pages just created

--- react-js/netflix-dupe/src/App.jsx
+++ react-js/netflix-dupe/src/App.jsx
@@ -1,11 +1,16 @@
+import {Routes, Route} from "react-router-dom"
+import Home from "./pages/Home"
+import Details from "./pages/Details"
+
 function App(){
 
   return (
     <>
     
-      <h1 className="text-3xl font-bold  text-center">
-        NetFlix Dupe
-      </h1>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/details/:slug" element={<Details />} />
+      </Routes>
 
     </> 
   )     
